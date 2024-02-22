da30dfbbfbdc3fd1d73f99b4f538ce51225bc945
alexgmzdev <contacto.alejandrogimenez@gmail.com>
2024-02-22 07:37:39 +0100

    Finalizados modo claro y oscura, implementacion de localStorage, solucionados errores en HTML

--- script-lgdkmode.js
+++ script-lgdkmode.js
@@ -3,4 +3,18 @@ const swith = document.querySelector(".switch");
 swith.addEventListener("click", e => {
     swith.classList.toggle("active");
     document.body.classList.toggle("active");
+
+    //Guardamos seleccion de usuario en LocalStorage
+    if(document.body.classList.contains('active')){
+        localStorage.setItem('light-mode', 'true');
+    } else{
+        localStorage.setItem('light-mode', 'false');
+    }
 })
+
+//Obtengo modo actual
+if(localStorage.getItem('light-mode')==='true'){
+    document.body.classList.add("active");
+} else{
+    document.body.classList.remove("active");
+}
