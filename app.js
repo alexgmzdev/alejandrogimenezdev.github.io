288dd7bd57c6b7fd81c17d1205900db02666f5dc
alexgmzdev <contacto.alejandrogimenez@gmail.com>
2024-02-23 19:25:08 +0100

    Bug fixeado: cierre del menú al pulsar seccion

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const menuItems = document.querySelectorAll('.navbar__item a');
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 })
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // Cerrar el menú móvil
+        menu.classList.remove('is-active');
+        menuLinks.classList.remove('active');
+    });
+});
